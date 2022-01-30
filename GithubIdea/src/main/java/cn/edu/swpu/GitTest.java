bcf056a120915b85ac537975647bc6fdf76a1b92
lizuodong <74031262+lizuodong@users.noreply.github.com>
2022-01-30 22:48:14 +0800

    pull commit

--- GithubIdea/src/main/java/cn/edu/swpu/GitTest.java
+++ GithubIdea/src/main/java/cn/edu/swpu/GitTest.java
@@ -3,5 +3,6 @@ package cn.edu.swpu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("pull test");
     }
 }
